570995fba9f3ac0cc02c0ae7f490c168698b53d5
午休 <shanshan.xue@hipac.cn>
2020-01-18 10:17:23 +0800

    changess

--- packages/module-1/test.js
+++ packages/module-1/test.js
@@ -1,1 +1,2 @@
 console.log('module-1 2223333ddddddd33')
+console.log('module-1 2223333ddddddd33')
--- packages/module-2/index.js
+++ packages/module-2/index.js
@@ -1,3 +1,4 @@
 exports.demo = (param) => {
     console.log('hello 123333333' + param);
+    console.log('hello 123333333' + param);
 }
