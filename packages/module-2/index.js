0330fbea11e43c2c42b90883bbd379acda81289e
午休 <shanshan.xue@hipac.cn>
2020-01-18 11:32:42 +0800

    test

--- packages/module-2/index.js
+++ packages/module-2/index.js
@@ -3,4 +3,5 @@ exports.demo = (param) => {
     console.log('hello 123333333' + param);
     console.log('hello 123333333' + param);
     console.log('hello 123333333' + param);
+    console.log('hello 123333333' + param);
 }
